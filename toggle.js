6699ee2ef50868f59488dd6645969407fbd809c7
sbasnet7227 <basnet_sunil@hotmail.com>
2019-10-19 02:14:39 -0700

    updated code

--- toggle.js
+++ toggle.js
@@ -1,22 +1,22 @@
 var button = document.querySelector("button");
-var isRed = false;
+// var isRed = false;
+// button.addEventListener("click", function(){
+    //if red, make it white
+    // if(isRed){
+    //     document.body.style.backgroundColor = 'white'; 
+    // }
+    // //else make it red
+    
+    // else {
+        
+    //     document.body.style.backgroundColor = 'red'; 
+    // }
+    // isRed = !isRed; // makes isRed inverted.
+// });
 button.addEventListener("click", function(){
-    //if white, make it red
-
-    if(isRed){
-        document.body.style.backgroundColor = 'white';
-        
-    }
-    //else make it white
     
-    else {
-        
-        document.body.style.backgroundColor = 'red';
+    document.body.classList.toggle('red');
 
-        
-    }
-    isRed = !isRed;
-
-    
 });
+
 
